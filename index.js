21bd4176924e550da033ef66ebe5856f1ab8b71d
Nick Gastis <nickgastis1@gmail.com>
2023-02-22 14:29:44 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+
+
+function distanceFromHqInBlocks(street) {
+    const scuber = 42
+    if (street < scuber) {
+    return scuber - street;
+    }
+    else {
+        return street - scuber;
+    }
+
+}
+
+function distanceFromHqInFeet(feetD) {
+    const blocks = distanceFromHqInBlocks(feetD);
+    const feet = blocks * 264;
+    return feet;
+  }
+
+function distanceTravelledInFeet(start, destination) {
+    const blockLength = 264;
+    const blockNum = (destination > start) ? (destination - start) : (start - destination)
+    const distanceInFeet = blockNum * blockLength;
+    return distanceInFeet;
+}
+
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
